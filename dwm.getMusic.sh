834e370adc29767b11168d91e260d80c47ef5ff3
Wjinlei <1976883731@qq.com>
2020-07-26 04:12:48 +0800

    update dwm.getMusic.sh

--- dwm.getMusic.sh
+++ dwm.getMusic.sh
@@ -6,12 +6,11 @@ pidof -x dwm.getMpd.sh >/dev/null 2>&1 || dwm.getMpd.sh >/dev/null 2>&1 &
 
 case $BLOCK_BUTTON in
 	1) mpc status | filter ; setsid -f "$TERMINAL" -e ncmpcpp ;;  # right click, pause/unpause
-	3) mpc toggle | filter ;;  # right click, pause/unpause
-	2) mpc status | filter ; notify-send "🎵 Music module" "\- Shows mpd song playing.
-- ⏸ when paused.
-- Left click opens ncmpcpp.
-- Middle click pauses.
-- Scroll changes track.";;  # right click, pause/unpause
+	2) mpc toggle | filter ;;  # right click, pause/unpause
+	3) mpc status | filter ; notify-send "🎵 Music module" "\- Shows mpd song playing.
+- 左键点击打开音乐列表.
+- 鼠标中键暂停/播放音乐.
+- 鼠标滚轮切换音乐.";;  # right click, pause/unpause
 	4) mpc prev   | filter ;;  # scroll up, previous
 	5) mpc next   | filter ;;  # scroll down, next
 	6) mpc status | filter ; "$TERMINAL" -e "$EDITOR" "$0" ;;
