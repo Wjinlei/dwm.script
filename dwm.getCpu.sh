0bea3449b7dc53533957827a88b2f352dc70537a
Wjinlei <1976883731@qq.com>
2020-07-27 23:02:36 +0800

    modify script

--- dwm.getCpu.sh
+++ dwm.getCpu.sh
@@ -3,7 +3,7 @@
 case $BLOCK_BUTTON in
 	1) notify-send "🖥 CPU hogs" "$(ps axch -o cmd:15,%cpu --sort=-%cpu | head)\\n(100% per core)" ;;
 	2) setsid -f "$TERMINAL" -e htop ;;
-	3) notify-send "🖥 CP模块" "\- 查看CPU温度.
+	3) notify-send "🖥 CPU模块" "\- 查看CPU温度.
 - 左键点击查看CPU占用率.
 - 中键点击打开htop." ;;
 	6) "$TERMINAL" -e "$EDITOR" "$0" ;;
--- dwm.getMusic.sh
+++ dwm.getMusic.sh
@@ -7,7 +7,7 @@ pidof -x dwm.getMpd.sh >/dev/null 2>&1 || dwm.getMpd.sh >/dev/null 2>&1 &
 case $BLOCK_BUTTON in
 	1) mpc status | filter ; setsid -f "$TERMINAL" -e ncmpcpp ;;  # right click, pause/unpause
 	2) mpc toggle | filter ;;  # right click, pause/unpause
-	3) mpc status | filter ; notify-send "🎵 Music module" "\- Shows mpd song playing.
+	3) mpc status | filter ; notify-send "🎵 音乐模块" "\- 显示正在播放的音乐.
 - 左键点击打开音乐列表.
 - 鼠标中键暂停/播放音乐.
 - 鼠标滚轮切换音乐.";;  # right click, pause/unpause
