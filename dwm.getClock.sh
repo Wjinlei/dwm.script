10bdc145a8b9b0de0e28857a5f6f49adcf2f84a1
Wjinlei <1976883731@qq.com>
2020-07-28 22:50:53 +0800

    update dwm.getClock.sh

--- dwm.getClock.sh
+++ dwm.getClock.sh
@@ -21,12 +21,12 @@ esac
 case $BLOCK_BUTTON in
 	1) notify-send "月份信息" "$(cal --color=always | sed "s/..7m/<b><span color=\"red\">/;s/..27m/<\/span><\/b>/")" && notify-send "Appointments" "$(calcurse -D ~/.config/calcurse -d3)" ;;
 	2) setsid -f "$TERMINAL" -e calcurse ;;
-	3) notify-send "📅 日期模块" "\- 查看月份信息
+	3) notify-send "🗓 日期模块" "\- 查看月份信息
 - 左键点击显示日历和未来三天的计划
 - 中键点击打开calcurse(如果已安装)" ;;
 	6) "$TERMINAL" -e "$EDITOR" "$0" ;;
 esac
 
-LOCALTIME=$(date +"📆:%Y-%m-%d(%a),${clock_icon}:%H:%M")
+LOCALTIME=$(date +"🗓:%Y-%m-%d(%a),${clock_icon}:%H:%M")
 printf "${LOCALTIME}"
 
