3455c023ef8db7d5a1a607122c7fe1614a25551d
Wjinlei <1976883731@qq.com>
2020-07-31 23:23:03 +0800

    update dwm.getBattery.sh

--- dwm.getBattery.sh
+++ dwm.getBattery.sh
@@ -19,7 +19,7 @@ for battery in /sys/class/power_supply/BAT?
 do
 	# Get its remaining capacity and charge status.
 	capacity=$(cat "$battery"/capacity 2>/dev/null) || break
-	status=$(sed "s/[Dd]ischarging/🔋/;s/[Nn]ot charging/🛑/;s/[Cc]harging/🔌/;s/[Uu]nknown/♻️/;s/[Ff]ull/😀/" "$battery"/status)
+	status=$(sed "s/[Dd]ischarging/🔋/;s/[Nn]ot charging/🛑/;s/[Cc]harging/🔌/;s/[Uu]nknown/❓/;s/[Ff]ull/😀/" "$battery"/status)
 
 	# If it is discharging and 25% or less, we will add a ❗ as a warning.
 	 [ "$capacity" -le 25 ] && [ "$status" = "🔋" ] && status="❗"
