06f5b769a451526ea9fe500206d9bf410edfaf30
Jerry Wang <freebufer.wang@gmail.com>
2022-03-29 00:43:05 +0800

    Update dwm.getVolume.sh

--- dwm.getVolume.sh
+++ dwm.getVolume.sh
@@ -5,8 +5,8 @@
 case $BLOCK_BUTTON in
 	1) setsid -f "$TERMINAL" -e pulsemixer ;;
 	2) pamixer -t ;;
-	4) pamixer --allow-boost -i 1 ;;
-	5) pamixer --allow-boost -d 1 ;;
+	#4) pamixer --allow-boost -i 1 ;;
+	#5) pamixer --allow-boost -d 1 ;;
 	3) notify-send "📢 音量模块" "\- 音量调节:🔇,🔈,🔉,🔊
 - 左键点击打开pulsemixer
 - 中键点击静音.
@@ -14,16 +14,20 @@ case $BLOCK_BUTTON in
 	6) "$TERMINAL" -e "$EDITOR" "$0" ;;
 esac
 
-vol=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')
-
-if [ "$vol" -eq "0" ]; then
+volume=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')
+if [ "$volume" -eq "0" ]; then
     icon="🔇"
-elif [ "$vol" -gt "70" ]; then
+elif [ "$volume" -gt "70" ]; then
 	icon="🔊"
-elif [ "$vol" -lt "30" ]; then
+elif [ "$volume" -lt "30" ]; then
 	icon="🔈"
 else
 	icon="🔉"
 fi
 
-echo "$icon:$vol%"
+toggle=$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)\].*/\1/')
+if [ "$enabled" == "off" ]; then
+    icon="🔇"
+fi
+
+echo "$icon:$volume%"
