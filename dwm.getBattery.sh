ea760f3b14b3e084b90547c15dd9f713673d1b83
Jerry Wang <freebufer.wang@gmail.com>
2022-03-29 01:11:48 +0800

    Update

--- dwm.getBattery.sh
+++ dwm.getBattery.sh
@@ -7,7 +7,7 @@ case $BLOCK_BUTTON in
 	3) notify-send "🔋 电源模块" "🔋: 使用电池中
 🛑: 未找到电池
 🔌: 充电中
-😀: 已充满
+🔋: 已充满
 ❗: 电量低于20%警告!" ;;
     #4) xbacklight -inc 10 ;;
     #5) xbacklight -dec 10 ;;
@@ -19,7 +19,7 @@ for battery in /sys/class/power_supply/BAT?
 do
 	# Get its remaining capacity and charge status.
 	capacity=$(cat "$battery"/capacity 2>/dev/null) || break
-	status=$(sed "s/[Dd]ischarging/🔋/;s/[Nn]ot charging/🛑/;s/[Cc]harging/🔌/;s/[Uu]nknown/❓/;s/[Ff]ull/😀/" "$battery"/status)
+	status=$(sed "s/[Dd]ischarging/🔋/;s/[Nn]ot charging/🛑/;s/[Cc]harging/🔌/;s/[Uu]nknown/❓/;s/[Ff]ull/🖥/" "$battery"/status)
 
 	# If it is discharging and 25% or less, we will add a ❗ as a warning.
 	 [ "$capacity" -le  5 ] && [ "$status" = "🔋" ] && status="❗" && poweroff
