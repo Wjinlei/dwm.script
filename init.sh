98e0fef3455d472f95827a8a9bfcac34bbf20bd2
Jerry Wang <freebufer.wang@gmail.com>
2022-04-07 20:37:19 +0800

    Delete some script and optimize

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fcitx5 -d
+dwmblocks &
+picom -b
+
+./wallpaper.sh &
+./chkUpdate &
+
--- rainbarf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-tmpstr=$(rainbarf --notmux --width 10)
-result=$(echo ${tmpstr} | cut -d'[' -f2 | cut -d'm' -f2)
-printf "$result"
-
